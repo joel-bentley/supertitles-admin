784553e13feb40f0f4c65de108ba101161daaa6c
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-03-24 19:03:53 -0400

    Add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,67 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Col,
+  FormControl,
+  ControlLabel,
+} from 'react-bootstrap';
 import { auth } from '../util/firebase';
 
-const Login = ({ history }) => (
-  <div className="container">
-    <h1>Login</h1>
-    <Button onClick={() => authorize(history)}>Press me to login</Button>
-  </div>
-);
+const Login = ({ history }) => {
+  let emailInput = null;
+  let passwordInput = null;
 
-function authorize(history) {
-  auth
-    .signInWithEmailAndPassword('jbben24@yahoo.com', 'testtesttest')
-    .then(console.log('Logged in'))
-    .then(() => history.push('/'))
-    .catch(error => console.log(error.message));
-}
+  const authorize = () => {
+    auth
+      .signInWithEmailAndPassword(emailInput.value, passwordInput.value)
+      .then(console.log('Logged in'))
+      .then(() => history.push('/'))
+      .catch(error => console.log(error.message));
+  };
+
+  return (
+    <div className="container">
+      <h1>Sign in to access control panel</h1>
+      <br />
+      <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Email
+          </Col>
+          <Col sm={10}>
+            <FormControl
+              type="email"
+              placeholder="Email"
+              inputRef={ref => emailInput = ref}
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={10}>
+            <FormControl
+              type="password"
+              placeholder="Password"
+              inputRef={ref => passwordInput = ref}
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button onClick={authorize}>
+              Sign in
+            </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
