72a0b92e9615ecdf51c94281a736b4c44393f345
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-03-24 19:02:42 -0400

    Modify env variable name

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -7,7 +7,7 @@ const config = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
