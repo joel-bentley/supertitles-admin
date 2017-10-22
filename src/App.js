64a287a01af500703a9837ecf21fce35aa342242
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-10-22 08:15:50 +0200

    Temporarily remove login screen

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends React.Component {
   state = { authState: false, error: null };
   componentDidMount() {
     this.updateAuthState();
+    if (!this.state.authState) {
+      this.authorize('test@test.com', 'testing');
+    }
   }
   updateAuthState = () => {
     this.setState({ authState: isAuthenticated() });
@@ -27,22 +30,28 @@ class App extends React.Component {
     }
   };
 
-  logout = async () => {
-    try {
-      await auth.signOut();
-      console.log('Logged out');
-      this.updateAuthState();
-      this.setState({ error: null });
-    } catch (error) {
-      console.log(error.message);
-    }
+  // logout = async () => {
+  //   try {
+  //     await auth.signOut();
+  //     console.log('Logged out');
+  //     this.updateAuthState();
+  //     this.setState({ error: null });
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
+
+  logout = () => {
+    console.log("You can't Log out ;)");
   };
+
   render() {
     const { authState, error } = this.state;
     return authState ? (
       <AdminPanel logout={this.logout} />
     ) : (
-      <Login authorize={this.authorize} error={error} />
+      <div />
+      // <Login authorize={this.authorize} error={error} />
     );
   }
 }
